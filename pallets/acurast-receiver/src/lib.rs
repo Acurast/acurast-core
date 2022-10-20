6103f2b4c84c3c04a66c2a22be56c5d6ffd64a85
Mike Godenzi <m.godenzi@papers.ch>
2022-10-20 09:27:05 +0200

    feat(acurast-receiver-pallet-generic-routing): modified interface to accept optional bytes

--- pallets/acurast-receiver/src/lib.rs
+++ pallets/acurast-receiver/src/lib.rs
@@ -15,64 +15,70 @@ mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use crate::traits::*;
-	use frame_support::pallet_prelude::*;
-	use frame_system::pallet_prelude::OriginFor;
+    use crate::traits::*;
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::OriginFor;
+    use sp_std::prelude::*;
 
-	/// Configure the pallet by specifying the parameters and types on which it depends.
-	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		/// Because this pallet emits events, it depends on the runtime's definition of an event.
-		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-		/// The fulfillment payload.
-		type Payload: Parameter
-			+ Member
-			+ MaxEncodedLen
-			+ Clone
-			+ Into<frame_support::inherent::Vec<u8>>;
-		/// Handler to notify the runtime when a new fulfillment is received.
-		type OnFulfillment: OnFulfillment<Self>;
-		/// Handle origin validation
-		type Barrier: ParachainBarrier<Self>;
-	}
+    /// Configure the pallet by specifying the parameters and types on which it depends.
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        /// Because this pallet emits events, it depends on the runtime's definition of an event.
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+        /// The fulfillment payload.
+        type Payload: Parameter + Member + Clone + Into<Vec<u8>>;
+        /// Generic parameters
+        type Parameters: Parameter + Member + Clone + Into<Vec<u8>>;
+        /// Handler to notify the runtime when a new fulfillment is received.
+        type OnFulfillment: OnFulfillment<Self>;
+        /// Handle origin validation
+        type Barrier: ParachainBarrier<Self>;
+    }
 
-	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
-	pub struct Pallet<T>(_);
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(super) trait Store)]
+    pub struct Pallet<T>(_);
 
-	/// TODO: We may want to add a storage for job identifiers, allowing acurast parachain to send
-	/// the (job identifer + payload). The requester address would be indexed by job identifier.
+    /// TODO: We may want to add a storage for job identifiers, allowing acurast parachain to send
+    /// the (job identifer + payload). The requester address would be indexed by job identifier.
 
-	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/v3/runtime/events-and-errors
-	#[pallet::event]
-	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {
-		FulfillReceived(T::Payload),
-	}
+    // Pallets use events to inform users when important changes are made.
+    // https://docs.substrate.io/v3/runtime/events-and-errors
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        FulfillReceived(T::Payload, Option<T::Parameters>),
+    }
 
-	// Errors inform users that something went wrong.
-	#[pallet::error]
-	pub enum Error<T> {}
+    // Errors inform users that something went wrong.
+    #[pallet::error]
+    pub enum Error<T> {}
 
-	#[pallet::call]
-	impl<T: Config> Pallet<T> {
-		/// Dispatchable function that notifies the runtime about a fulfilment coming from acurast parachain.
-		#[pallet::weight(Weight::from_ref_time(10_000).saturating_add(T::DbWeight::get().writes(1)))]
-		pub fn fulfill(origin: OriginFor<T>, payload: T::Payload) -> DispatchResult {
-			// Check that the extrinsic comes from a trusted xcm channel.
-			T::Barrier::ensure_xcm_origin(origin)?;
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        /// Dispatchable function that notifies the runtime about a fulfilment coming from acurast parachain.
+        #[pallet::weight(Weight::from_ref_time(10_000).saturating_add(T::DbWeight::get().writes(1)))]
+        pub fn fulfill(
+            origin: OriginFor<T>,
+            payload: T::Payload,
+            parameters: Option<T::Parameters>,
+        ) -> DispatchResult {
+            // Check that the extrinsic comes from a trusted xcm channel.
+            T::Barrier::ensure_xcm_origin(origin)?;
 
-			// Notify the runtime about the fulfillment.
-			match T::OnFulfillment::fulfill(&payload.clone().into()) {
-				Err(err) => Err(err.error),
-				Ok(_) => {
-					// Emit events
-					Self::deposit_event(Event::FulfillReceived(payload));
+            // Notify the runtime about the fulfillment.
+            match T::OnFulfillment::fulfill(
+                payload.clone().into(),
+                parameters.clone().map(|parameters| parameters.into()),
+            ) {
+                Err(err) => Err(err.error),
+                Ok(_) => {
+                    // Emit events
+                    Self::deposit_event(Event::FulfillReceived(payload, parameters));
 
-					Ok(())
-				}
-			}
-		}
-	}
+                    Ok(())
+                }
+            }
+        }
+    }
 }
--- pallets/acurast-receiver/src/tests.rs
+++ pallets/acurast-receiver/src/tests.rs
@@ -1,33 +1,33 @@
 use crate::mock::*;
-use frame_support::{assert_err, assert_ok, traits::ConstU32, BoundedVec};
+use frame_support::{assert_err, assert_ok};
 use xcm::v2::{Junction::Parachain, Junctions::X1, MultiLocation};
 
 #[test]
 fn test_fulfill() {
-	let payload: BoundedVec<u8, ConstU32<128>> = vec![0; 128].try_into().unwrap();
+    let payload = vec![0u8; 128];
 
-	new_test_ext().execute_with(|| {
-		// Mock and xcm origin
-		let xcm_origin = Origin::from(pallet_xcm::Origin::Xcm(MultiLocation {
-			parents: 1,
-			interior: X1(Parachain(2001)),
-		}));
+    new_test_ext().execute_with(|| {
+        // Mock and xcm origin
+        let xcm_origin = Origin::from(pallet_xcm::Origin::Xcm(MultiLocation {
+            parents: 1,
+            interior: X1(Parachain(2001)),
+        }));
 
-		// Dispatch fulfill extrinsic with valid origin.
-		assert_ok!(AcurastReceiver::fulfill(xcm_origin, payload.clone()));
-	});
+        // Dispatch fulfill extrinsic with valid origin.
+        assert_ok!(AcurastReceiver::fulfill(xcm_origin, payload.clone(), None));
+    });
 
-	new_test_ext().execute_with(|| {
-		// Mock and xcm origin
-		let xcm_origin = Origin::from(pallet_xcm::Origin::Xcm(MultiLocation {
-			parents: 1,
-			interior: X1(Parachain(2000)),
-		}));
+    new_test_ext().execute_with(|| {
+        // Mock and xcm origin
+        let xcm_origin = Origin::from(pallet_xcm::Origin::Xcm(MultiLocation {
+            parents: 1,
+            interior: X1(Parachain(2000)),
+        }));
 
-		// Dispatch fulfill extrinsic with wrong origin.
-		assert_err!(
-			AcurastReceiver::fulfill(xcm_origin, payload.clone()),
-			"MultiLocation not allowed."
-		);
-	});
+        // Dispatch fulfill extrinsic with wrong origin.
+        assert_err!(
+            AcurastReceiver::fulfill(xcm_origin, payload.clone(), None),
+            "MultiLocation not allowed."
+        );
+    });
 }
