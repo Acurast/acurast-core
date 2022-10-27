1c6385466c922dbc0212c3bbde30a4c9b3dbe635
Rodrigo Quelhas <r.quelhas@papers.ch>
2022-10-27 10:56:09 +0100

    feat(fee): Support fee management on acurast pallet

--- pallets/acurast/src/lib.rs
+++ pallets/acurast/src/lib.rs
@@ -12,12 +12,14 @@ mod attestation;
 pub mod payments;
 mod types;
 mod utils;
+mod traits;
 pub mod weights;
 pub mod xcm_adapters;
 
 pub use pallet::*;
 pub use payments::*;
 pub use types::*;
+pub use traits::*;
 
 #[frame_support::pallet]
 pub mod pallet {
@@ -31,59 +33,7 @@ pub mod pallet {
     use crate::payments::*;
     use crate::types::*;
     use crate::utils::*;
-
-    /// This trait provides the interface for a fulfillment router.
-    pub trait FulfillmentRouter<T: Config> {
-        fn received_fulfillment(
-            origin: OriginFor<T>,
-            from: T::AccountId,
-            fulfillment: Fulfillment,
-            registration: JobRegistration<T::AccountId, T::RegistrationExtra>,
-            requester: <T::Lookup as StaticLookup>::Target,
-        ) -> DispatchResultWithPostInfo;
-    }
-
-    pub trait RevocationListUpdateBarrier<T: Config> {
-        fn can_update_revocation_list(
-            origin: &T::AccountId,
-            updates: &Vec<CertificateRevocationListUpdate>,
-        ) -> bool;
-    }
-
-    impl<T: Config> RevocationListUpdateBarrier<T> for () {
-        fn can_update_revocation_list(
-            _origin: &T::AccountId,
-            _updates: &Vec<CertificateRevocationListUpdate>,
-        ) -> bool {
-            false
-        }
-    }
-
-    pub trait JobAssignmentUpdateBarrier<T: Config> {
-        fn can_update_assigned_jobs(
-            origin: &T::AccountId,
-            updates: &Vec<JobAssignmentUpdate<T::AccountId>>,
-        ) -> bool;
-    }
-
-    impl<T: Config> JobAssignmentUpdateBarrier<T> for () {
-        fn can_update_assigned_jobs(
-            _origin: &T::AccountId,
-            _updates: &Vec<JobAssignmentUpdate<T::AccountId>>,
-        ) -> bool {
-            false
-        }
-    }
-
-    pub trait WeightInfo {
-        fn register() -> Weight;
-        fn deregister() -> Weight;
-        fn update_allowed_sources() -> Weight;
-        fn update_job_assignments() -> Weight;
-        fn fulfill() -> Weight;
-        fn submit_attestation() -> Weight;
-        fn update_certificate_revocation_list() -> Weight;
-    }
+    use crate::traits::*;
 
     #[pallet::config]
     pub trait Config:
@@ -106,7 +56,9 @@ pub mod pallet {
         type RevocationListUpdateBarrier: RevocationListUpdateBarrier<Self>;
         /// Barrier for update_job_assignments extrinsic call.
         type JobAssignmentUpdateBarrier: JobAssignmentUpdateBarrier<Self>;
-
+        // Fee Logic
+        type FeeManager: FeeManager;
+        // Weight Logic
         type WeightInfo: WeightInfo;
     }
 
