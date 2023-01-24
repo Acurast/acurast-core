51af1b80f410e22ec7ddcd559f7ae15498fa5c09
Simon Wehrli <simon@smartnuance.com>
2023-01-24 20:50:42 +0100

    genesis template for pallet_acurast_assets
    cannot list asset mapping until xcm structs are deserializable

--- pallets/assets/src/lib.rs
+++ pallets/assets/src/lib.rs
@@ -10,7 +10,6 @@
 // mod benchmarking;
 pub mod weights;
 
-use frame_support::{dispatch::Weight, traits::Get};
 use sp_runtime::traits::StaticLookup;
 
 pub use pallet::*;
@@ -25,6 +24,7 @@ pub mod pallet {
     use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::*;
     use xcm::latest::MultiLocation;
+    use xcm::prelude::{AssetId, GeneralIndex, PalletInstance, Parachain, X3};
 
     #[pallet::pallet]
     #[pallet::generate_store(pub(super) trait Store)]
@@ -40,15 +40,42 @@ pub mod pallet {
         type WeightInfo: WeightInfo;
     }
 
+    #[pallet::genesis_config]
+    pub struct GenesisConfig<T: Config<I>, I: 'static = ()> {
+        /// Genesis assets: `internal asset ID -> asset ID` (Statemint's general index)
+        // TODO generalize asset ID to any XCM AssetID once structs derive deserialize (merged with XCM-3)
+        pub assets: Vec<(<T as pallet_assets::Config<I>>::AssetId, u32)>,
+    }
+
+    #[cfg(feature = "std")]
+    impl<T: Config<I>, I: 'static> Default for GenesisConfig<T, I> {
+        fn default() -> Self {
+            Self{
+                assets: vec![],
+            }
+        }
+    }
+
+    #[pallet::genesis_build]
+    impl<T: Config<I>, I: 'static> GenesisBuild<T, I> for GenesisConfig<T, I> {
+        fn build(&self) {
+            for &(internal_asset_id, general_index) in &self.assets {
+                let asset_id = AssetId::Concrete(MultiLocation::new(1, X3(Parachain(1000), PalletInstance(50), GeneralIndex(general_index as u128))));
+                assert!(!<AssetIndex<T, I>>::contains_key(internal_asset_id), "Asset internal id already in use");
+                assert!(!<ReverseAssetIndex<T, I>>::contains_key(asset_id), "Asset id already in use");
+            }
+        }
+    }
+
     #[pallet::storage]
     #[pallet::getter(fn asset_index)]
     pub type AssetIndex<T: Config<I>, I: 'static = ()> =
-        StorageMap<_, Blake2_128, <T as pallet_assets::Config<I>>::AssetId, MultiLocation>;
+        StorageMap<_, Blake2_128, <T as pallet_assets::Config<I>>::AssetId, AssetId>;
 
     #[pallet::storage]
     #[pallet::getter(fn reverse_asset_index)]
     pub type ReverseAssetIndex<T: Config<I>, I: 'static = ()> =
-        StorageMap<_, Blake2_128, MultiLocation, <T as pallet_assets::Config<I>>::AssetId>;
+        StorageMap<_, Blake2_128, AssetId, <T as pallet_assets::Config<I>>::AssetId>;
 
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
@@ -69,16 +96,32 @@ pub mod pallet {
     where
         T: pallet_assets::Config<I>,
     {
-        /// Creates and indexes a bijective mapping `id <-> native_id` and proxies to [`pallet_assets::Pallet::force_create()`].
+        #[pallet::call_index(0)]
+        #[pallet::weight(<T as Config<I>>::WeightInfo::create())]
+        pub fn create(
+            origin: OriginFor<T>,
+            id: T::AssetIdParameter,
+            admin: AccountIdLookupOf<T>,
+            min_balance: T::Balance,
+        ) -> DispatchResult {
+            <pallet_assets::Pallet<T, I>>::create(
+                origin,
+                id,
+                admin,
+                min_balance,
+            )
+        }
+
+        /// Creates and indexes a bijective mapping `id <-> internal_id` and proxies to [`pallet_assets::Pallet::force_create()`].
         ///
         /// This extrinsic is idempotent when used with the same `id` and `asset` (does not receate the asset in `pallet_asset`.
         /// Trying to index an already indexed asset or using the same id to index a different asset results in an error.
-        #[pallet::call_index(0)]
+        #[pallet::call_index(1)]
         #[pallet::weight(<T as Config<I>>::WeightInfo::create())]
         pub fn force_create(
             origin: OriginFor<T>,
             id: <T as pallet_assets::Config<I>>::AssetIdParameter,
-            asset: MultiLocation,
+            asset: AssetId,
             owner: AccountIdLookupOf<T>,
             is_sufficient: bool,
             min_balance: T::Balance,
@@ -116,7 +159,7 @@ pub mod pallet {
         #[pallet::weight(<T as Config<I>>::WeightInfo::set_metadata(name.len() as u32, symbol.len() as u32))]
         pub fn set_metadata(
             origin: OriginFor<T>,
-            id: MultiLocation,
+            id: AssetId,
             name: Vec<u8>,
             symbol: Vec<u8>,
             decimals: u8,
@@ -129,7 +172,7 @@ pub mod pallet {
         #[pallet::weight(<T as Config<I>>::WeightInfo::transfer())]
         pub fn transfer(
             origin: OriginFor<T>,
-            id: MultiLocation,
+            id: AssetId,
             target: AccountIdLookupOf<T>,
             #[pallet::compact] amount: T::Balance,
         ) -> DispatchResult {
@@ -141,7 +184,7 @@ pub mod pallet {
         #[pallet::weight(<T as Config<I>>::WeightInfo::force_transfer())]
         pub fn force_transfer(
             origin: OriginFor<T>,
-            id: MultiLocation,
+            id: AssetId,
             source: AccountIdLookupOf<T>,
             dest: AccountIdLookupOf<T>,
             #[pallet::compact] amount: T::Balance,
