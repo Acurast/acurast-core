5d74b4ef9f6075aac254ac3455b793c92c342e93
Simon Wehrli <simon@smartnuance.com>
2023-10-16 10:25:27 +0100

    feat: parameterize set_environment benchmarks

--- pallets/acurast/src/traits.rs
+++ pallets/acurast/src/traits.rs
@@ -42,7 +42,7 @@ pub trait WeightInfo {
     fn update_allowed_sources(x: u32) -> Weight;
     fn submit_attestation() -> Weight;
     fn update_certificate_revocation_list() -> Weight;
-    fn set_environment() -> Weight;
+    fn set_environment(x: u32) -> Weight;
 }
 
 /// Allows to hook additional logic for various job related extrinsics.
