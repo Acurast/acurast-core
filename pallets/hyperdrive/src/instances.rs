87cfc0d8296d89d5d92dd6ffcfe2717ba7e0e39e
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2023-12-12 14:52:24 +0000

    feat: add aleph zero instance

--- pallets/hyperdrive/src/instances.rs
+++ pallets/hyperdrive/src/instances.rs
@@ -1,4 +1,4 @@
-use frame_support::instances::{Instance1, Instance2};
+use frame_support::instances::{Instance1, Instance2, Instance3};
 use frame_support::pallet_prelude::{Decode, Encode};
 use scale_info::TypeInfo;
 use sp_core::RuntimeDebug;
@@ -7,10 +7,12 @@ use sp_core::RuntimeDebug;
 pub enum HyperdriveInstance {
     Tezos,
     Ethereum,
+    AlephZero,
 }
 
 pub type TezosInstance = Instance1;
 pub type EthereumInstance = Instance2;
+pub type AlephZeroInstance = Instance3;
 
 pub trait HyperdriveInstanceName {
     const NAME: HyperdriveInstance;
@@ -22,4 +24,8 @@ impl HyperdriveInstanceName for TezosInstance {
 
 impl HyperdriveInstanceName for EthereumInstance {
     const NAME: HyperdriveInstance = HyperdriveInstance::Ethereum;
+}
+
+impl HyperdriveInstanceName for AlephZeroInstance {
+    const NAME: HyperdriveInstance = HyperdriveInstance::AlephZero;
 }
